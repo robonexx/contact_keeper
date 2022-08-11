01d0ecf0b0ec9f77869958b48c52f90ab2d9fdb7
robonexx <robertwagar@gmail.com>
2022-08-11 12:30:57 +0200

    contact item styling

--- client/src/components/contacts/ContactItem.js
+++ client/src/components/contacts/ContactItem.js
@@ -1,13 +1,26 @@
 import React from 'react'
+import {RiMailOpenLine, RiPhoneLine} from 'react-icons/ri'
 
 const ContactItem = ({contact}) => {
     const {id, name, email, phone, type} = contact;
   return (
     <div className="card bg-light">
         <h3 className='text-primary text-left'>
-            {name} {' '}<span className={`badge ${type === 'professional' ? 'badge-success' : 'badge-primary'}`}>{type}</span>
+            {name} {' '}<span 
+            style={{float: 'right'}}
+            className={`badge ${type === 'professional' ? 'badge-success' : 'badge-primary'}`}>{type.charAt(0).toUpperCase() + type.slice(1)}</span>
         </h3>
-
+          <ul className='list'>
+            {email && (
+              <li style={{display: 'flex', alignItems: 'center'}}>
+                <RiMailOpenLine />{' '}{email}
+              </li>)}
+            {phone && (
+              <li style={{display: 'flex', alignItems: 'center'}}>
+                <RiPhoneLine />{' '}{phone}
+              </li>
+            )}
+          </ul>
     </div>
   )
 }
