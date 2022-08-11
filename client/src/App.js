787a1e02b899bbe3b0141f4bc14ccbc266650021
robonexx <robertwagar@gmail.com>
2022-08-11 11:08:44 +0200

    some styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
 import './App.scss';
+import Navbar from './components/layout/navbar/Navbar';
+import { RiContactsBook2Line } from 'react-icons/ri'
 
 function App() {
   return (
     <div className="App">
+      <Navbar icon={<RiContactsBook2Line />}/>
     <h1>Clean repo</h1>
     </div>
   );
